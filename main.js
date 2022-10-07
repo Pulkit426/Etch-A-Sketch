4e254df1d5736a3f170a5fe7df7def7c49ea4fb2
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-10-07 22:47:10 +0530

    add color functionality

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const gridContainer= document.querySelector('.grid-container')
 for(let i=0;i<256;i++){
     const gridDiv = document.createElement('div')
     gridDiv.classList.add('grid-item')
+    gridDiv.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black'
+    })
     gridContainer.appendChild(gridDiv)
 }
-
-console.log(gridContainer)
